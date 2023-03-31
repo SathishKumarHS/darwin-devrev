48e1e62fd5bbb174f534d3214c70ab186a540ee4
SathishKumarHS <i-Sathish.KumarHS@devrev.ai>
2023-03-31 14:57:13 +0530

    Add Homebrew formula for v1.0.3

--- darwin-devrev.rb
+++ darwin-devrev.rb
@@ -1,7 +1,7 @@
 class darwin-devrev < Formula
     desc "A brief description of darwin-devrev"
     homepage "https://github.com/SathishKumarHS/darwin-devrev"
-    url "https://github.com/SathishKumarHS/darwin-devrev/releases/download/v1.0.0/darwin-devrev_1.0.0_darwin_amd64.tar.gz"
+    url "https://github.com/SathishKumarHS/darwin-devrev/releases/download/v1.0.3/darwin-devrev_1.0.3_darwin_amd64.tar.gz"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
     def install
